bdcbc493907233a14ddb0dbea8c1775c2c283187
Paul K <spkim0921@gmail.com>
2025-01-03 14:43:01 -0800

    11th commit

--- frontend/src/pages/DietaryPage.tsx
+++ frontend/src/pages/DietaryPage.tsx
@@ -9,7 +9,9 @@ export default function DietaryPage() {
     const { invitee, setInvitee, currentInviteeId, setCurrentInviteeId } =
         useInviteeStore((state) => state);
     console.log(invitee);
-    const [dietaryContent, setDietaryContent] = useState(invitee.dietary);
+    const [dietaryContent, setDietaryContent] = useState(
+        invitee ? invitee.dietary : ""
+    );
 
     async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
@@ -33,38 +35,51 @@ export default function DietaryPage() {
 
     return (
         <div>
-            <div className="font-bold my-5">
-                Responding for {invitee.first_name}
-            </div>
-            <div>Do you have any dietary restrictions and/or allergies?</div>
-            <div>Please list below, otherwise leave blank.</div>
-            <form onSubmit={handleSubmit} className="flex flex-col">
-                <textarea
-                    name=""
-                    id=""
-                    value={dietaryContent}
-                    onChange={(e) => setDietaryContent(e.target.value)}
-                    className="bg-[#E6E0E9] m-5 md:w-[500px] h-[100px] border border-b-gray-400 mx-auto"
-                ></textarea>
-                <div className="flex mx-auto">
-                    <NavLink
-                        to="/rsvp"
-                        className="px-5 py-2 mr-2 bg-[#65558F] text-white rounded-full"
-                    >
-                        Go Back
-                    </NavLink>
-                    <button className="px-5 py-2 ml-2 bg-[#65558F] text-white rounded-full">
-                        Next
-                    </button>
+            {invitee ? (
+                <div>
+                    <div className="font-bold my-5">
+                        Responding for {invitee.first_name}
+                    </div>
+                    <div>
+                        Do you have any dietary restrictions and/or allergies?
+                    </div>
+                    <div>Please list below, otherwise leave blank.</div>
+                    <form onSubmit={handleSubmit} className="flex flex-col">
+                        <textarea
+                            name=""
+                            id=""
+                            value={dietaryContent}
+                            onChange={(e) => setDietaryContent(e.target.value)}
+                            className="bg-[#E6E0E9] m-5 md:w-[500px] h-[100px] border border-b-gray-400 mx-auto"
+                        ></textarea>
+                        <div className="flex mx-auto">
+                            <NavLink
+                                to="/rsvp"
+                                className="px-5 py-2 mr-2 bg-[#65558F] text-white rounded-full"
+                            >
+                                Go Back
+                            </NavLink>
+                            <button className="px-5 py-2 ml-2 bg-[#65558F] text-white rounded-full">
+                                Next
+                            </button>
+                        </div>
+                    </form>
+                    <div>
+                        <div className="absolute bottom-[22%] left-[50%] pt-44 pb-3 text-center">
+                            2/3
+                        </div>
+                        <div className="absolute bottom-[20%] left-10 md:left-[10%] w-[80%] bg-[#d9d9d9] h-2"></div>
+                        <div className="absolute bottom-[20%] left-10 md:left-[10%] w-[54%] bg-[#65558F] h-2"></div>
+                    </div>
                 </div>
-            </form>
-            <div>
-                <div className="absolute bottom-[22%] left-[50%] pt-44 pb-3 text-center">
-                    2/3
+            ) : (
+                <div>
+                    <div>Whoops! Something went wrong :(</div>
+                    <div className="x-5 py-2 my-2 bg-[#65558F] text-white rounded-full w-[100px] mx-auto">
+                        <NavLink to="/">Go Back</NavLink>
+                    </div>
                 </div>
-                <div className="absolute bottom-[20%] left-10 md:left-[10%] w-[80%] bg-[#d9d9d9] h-2"></div>
-                <div className="absolute bottom-[20%] left-10 md:left-[10%] w-[54%] bg-[#65558F] h-2"></div>
-            </div>
+            )}
         </div>
     );
 }
